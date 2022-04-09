c286ce0973025597b1dfcafc9d2184b4da915dba
校长 <zzy0523@gmail.com>
2022-04-09 14:59:26 +0800

    修复PagingListAdapter支持

--- app/src/main/java/com/julive/adapter_demo/paging/Model.kt
+++ app/src/main/java/com/julive/adapter_demo/paging/Model.kt
@@ -1,9 +1,8 @@
 package com.julive.adapter_demo.paging
 
 import androidx.lifecycle.ViewModel
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.PagingSource
+import androidx.lifecycle.viewModelScope
+import androidx.paging.*
 import com.julive.adapter.core.LayoutViewModel
 import com.julive.adapter.core.SameModel
 import com.julive.adapter_demo.R
@@ -14,34 +13,36 @@ class PagingModelTest(val title: String, override var uniqueId: String = title) 
 
 class PagingViewModel : ViewModel() {
 
-//    val pager by lazy {
-//        Pager(
-//            config = PagingConfig(pageSize = 3, prefetchDistance = 1),
-//            pagingSourceFactory = { MainSource() }).flow
-//    }
+    val pager by lazy {
+        Pager(
+            config = PagingConfig(pageSize = 3, prefetchDistance = 1),
+            pagingSourceFactory = { MainSource() }).flow.cachedIn(viewModelScope)
+    }
 
-//    class MainSource : PagingSource<Int, LayoutViewModel<PagingModelTest>>() {
-//        override suspend fun load(params: LoadParams<Int>): LoadResult<Int, LayoutViewModel<PagingModelTest>> {
-//            // 如果key是null，那就加载第0页的数据
-//            val page = params.key ?: 0
-//            // 每一页的数据长度
-//            val pageSize = params.loadSize
-//            return try {
-//                delay(2000)
-//                val data = mutableListOf<LayoutViewModel<PagingModelTest>>()
-//                val itemPageViewModel = LayoutViewModel<PagingModelTest>(R.layout.item_test)
-//                itemPageViewModel.model = PagingModelTest("标题")
-//                data.add(itemPageViewModel)
-//                data.add(itemPageViewModel)
-//                LoadResult.Page(
-//                    data = data,
-//                    prevKey = if (page == 0) null else page - 1,
-//                    nextKey = page + 1
-//                )
-//            } catch (e: Exception) {
-//                LoadResult.Error(e)
-//            }
-//
-//        }
-//    }
+    class MainSource : PagingSource<Int, LayoutViewModel<PagingModelTest>>() {
+        override suspend fun load(params: LoadParams<Int>): LoadResult<Int, LayoutViewModel<PagingModelTest>> {
+            // 如果key是null，那就加载第0页的数据
+            val page = params.key ?: 0
+            // 每一页的数据长度
+            val pageSize = params.loadSize
+            return try {
+                delay(2000)
+                val data = mutableListOf<LayoutViewModel<PagingModelTest>>()
+                val itemPageViewModel = LayoutViewModel<PagingModelTest>(R.layout.item_test)
+                itemPageViewModel.model = PagingModelTest("标题")
+                data.add(itemPageViewModel)
+                LoadResult.Page(
+                    data = data,
+                    prevKey = if (page == 0) null else page - 1,
+                    nextKey = page + 1
+                )
+            } catch (e: Exception) {
+                LoadResult.Error(e)
+            }
+        }
+
+        override fun getRefreshKey(state: PagingState<Int, LayoutViewModel<PagingModelTest>>): Int? {
+            return state.anchorPosition
+        }
+    }
 }
--- app/src/main/java/com/julive/adapter_demo/paging/Paging3Activity.kt
+++ app/src/main/java/com/julive/adapter_demo/paging/Paging3Activity.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.paging.ExperimentalPagingApi
 import androidx.paging.LoadState
+import androidx.paging.PagingDataAdapter
 import com.julive.adapter.core.into
 import com.julive.adapter.paging.PagingListAdapter
 import com.julive.adapter.paging.PagingLoadStateAdapter
@@ -15,6 +16,8 @@ import com.julive.adapter_demo.main.setBtnText
 import kotlinx.android.synthetic.main.activity_paging3.*
 import kotlinx.android.synthetic.main.include_button_bottom.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class Paging3Activity : AppCompatActivity() {
@@ -39,18 +42,18 @@ class Paging3Activity : AppCompatActivity() {
         mPagingListAdapter.withLoadStateFooter(PagingLoadStateAdapter()).into(rv_paging_list)
 
         lifecycleScope.launch {
-//            viewModel.pager.collect {
-//                mPagingListAdapter.submitData(it)
-//            }
+            viewModel.pager.collect {
+                mPagingListAdapter.submitData(it)
+            }
         }
 
         mPagingListAdapter.addLoadStateListener { loadStates ->
             btn_right.isVisible = loadStates.append is LoadState.Error
-//            refresh_paging_layout.isRefreshing = loadStates.refresh is LoadState.Loading
+            refresh_paging_layout.isRefreshing = loadStates.refresh is LoadState.Loading
         }
 
-        btn_left.setText("新增")
-        btn_middle.setText("删除")
+//        btn_left.setText("新增")
+//        btn_middle.setText("删除")
         btn_right.setBtnText("更新").setOnClickListener {
                 mPagingListAdapter.refresh()
         }
